5785c0c272a44b2decd6ce539bd67783c73a47e3
MaksMalf <maksim.malofeev995@gmail.com>
2022-08-28 22:46:02 +0300

    Initial Commit

---
+++ MyCoreDataProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCoreDataProject
+//
+//  Created by Maksim Malofeev on 28/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
